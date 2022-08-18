30b757804d7a631e9e016fc66aa0f231222c4f6c
Bayan Jones <59375928+bayanalan@users.noreply.github.com>
2022-08-17 19:41:21 -0700

    added deploy

--- src/lib/components/screenwidth.svelte
+++ src/lib/components/screenwidth.svelte
@@ -1,4 +1,6 @@
-<!-- <div class="w-min h-4 text-xs text-center fixed top-0 -left-1 px-2 -my-1 text-white bg-gray-500 rounded-lg overflow-hidden">
+<!-- 
+  
+  <div class="w-min h-4 text-xs text-center fixed top-0 -left-1 px-2 -my-1 text-white bg-gray-500 rounded-lg overflow-hidden">
     <span class="inline sm:hidden">xsm</span>
     <span class="hidden sm:inline md:hidden">sm</span>
     <span class="hidden md:inline lg:hidden">md</span>
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,20 @@
 import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-auto";
-
-/** @type {import('@sveltejs/kit').Config} */
+import adapter from '@sveltejs/adapter-static';
+ 
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter({
+      // default options are shown. On some platforms
+      // these options are set automatically — see below
+      pages: 'build',
+      assets: 'build',
+      fallback: null,
+      precompress: false
+    }),
+    prerender: {
+      // This can be false if you're using a fallback (i.e. SPA mode)
+      default: true
+    },
   },
 
   preprocess: [
