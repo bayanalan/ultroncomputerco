95dde2da10dc242edd2521ea567c6623f84765f6
Bayan Jones <59375928+bayanalan@users.noreply.github.com>
2022-08-28 08:51:20 -0700

    New dark theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,31 @@
-<div class="navbar bg-black sticky top-1 rounded-full w-[calc(100%-20px)] mx-auto">
+<div class="navbar bg-white sticky top-1 rounded-full w-[calc(100%-20px)] mx-auto">
     <div class="navbar-center text-center mx-auto ">
       <!-- svelte-ignore a11y-missing-attribute -->
-      <a class="text-white text-3xl ">
+      <a class="text-black text-3xl ">
         Ultron Computer
-        <span class="inline sm:hidden"> Co.</span>
-        <span class="hidden sm:inline"> Company</span>
+        <span class="poppins inline sm:hidden"> Co.</span>
+        <span class="poppins hidden sm:inline"> Company</span>
       </a>
     </div>
   </div>
   
- <h1 class="text-center text-6xl pt-12">About Me</h1> 
+ <h1 class=" poppins text-center text-6xl pt-12">About Me</h1> 
 <p class="text-center pt-10" > My name is Bayan Jones and I am nine years old. Ultron Computer Co is my own Computer Company. I really like making computers, and that's
  why I started the Ultron Computer Co.</p>
  <h1 class="text-6xl text-center font-Roboto pt-14">UltronBook</h1>
  <h1 class="pt-5 text-center">The UltronBook is a Laptop which I designed in tinkercad. It has a 13 inch display,
-runs android 12 Samsung DeX, 8gb RAM, 5 megapixel camera, 128GB Built in storage, 3 fans, and a 1TB Micro SD Card. I have the Galaxy S10, the display, and the trackpad. I apologize if the Micro SD Card was $184.25 </h1>
+runs android 12 Samsung DeX, 8gb RAM, 5 megapixel camera, 128GB Built in storage, 3 fans, 8 usb ports, USB-C charging, a 3.5mm jack, and a 1TB Micro SD Card. I have the Galaxy S10, the display, and the trackpad. I apologize if the Micro SD Card was $184.25 </h1>
 
-<div class="w-full text-center">
-  <a class="btn mt-6 rounded-full bg-black text-white hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/3EDWSS4QYWYUV/ref=nav_wishlist_lists_3" target="_blank">UltronBook Wishlist</a>
+<div class=" poppins w-full text-center">
+  <a class="btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/3EDWSS4QYWYUV/ref=nav_wishlist_lists_3" target="_blank">UltronBook Wishlist</a>
 </div>
 
-<h1 class="text-center text-6xl pt-14">EZELEA</h1>
+<h1 class=" poppins text-center text-6xl pt-14">EZELEA</h1>
 
-<h1 class="text-center pt-5">You might be thinking that I am crazy for naming the next big thing after a company that makes showerheads, but I make some computers out of boxes. The EZELEA showerhead box has a magnetic lid to open it. 1: I thought the lid was good for upgrading storage, 2: I didn't want to hot glue paper to the top of the computer to put an Ultron logo on it and call it the GameBox 2. I did hot glue paper to the bottom which actually looks pretty good. The EZELEA will be a retro gaming device smaller that a Xbox Series X! I already have the Raspberry Pi 4 8GB and the 256GB Micro SD Card. </h1>
+<h1 class="text-center pt-5">You might be thinking that I am crazy for naming the next big thing after a company that makes showerheads, but I make some computers out of boxes. The EZELEA showerhead box has a magnetic lid to open it. 1: I thought the lid was good for upgrading storage, 2: I didn't want to hot glue paper to the top of the computer to put an Ultron logo on it and call it the GameBox 2. I did hot glue paper to the bottom which actually looks pretty good. The EZELEA will be a retro gaming device smaller that a Xbox Series X! I already have the Raspberry Pi 4 8GB and the 256GB Micro SD Card. It also has No usb port. The controller is wireless with a dongle inside.</h1>
 
 <div class="w-full text-center">
-  <a class="btn mt-6 rounded-full bg-black text-white hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/3CB3750IQR8GK?ref_=wl_share" target="_blank">EZELEA Wishlist</a>
+  <a class=" poppins btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/3CB3750IQR8GK?ref_=wl_share" target="_blank">EZELEA Wishlist</a>
 </div>
 
 <h1 class="text-center text-6xl pt-14">Dream PC</h1>
@@ -33,18 +33,31 @@ runs android 12 Samsung DeX, 8gb RAM, 5 megapixel camera, 128GB Built in storage
 <h1 class="text-center pt-5">I included my Dream PC because I want to develop websites with code, (you need a powerful computer to do that), play games and make videos about it, (you also need a powerful computer to do that), and I want to use it for tinkercad models of Ultron computers. I already have the Aorus B450 Pro Motherboard</h1>
 
 <div class="w-full text-center">
-  <a class="btn mt-6 rounded-full bg-black text-white hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/W7807HTP7TBW?ref_=wl_share" target="_blank">Dream PC Wishlist</a>
+  <a class=" poppins btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/W7807HTP7TBW?ref_=wl_share" target="_blank">Dream PC Wishlist</a>
 </div>
-<h1 class="pt-24"> </h1>
+ 
+<h1 class="text-center text-6xl pt-14 ">Ultron Desktop </h1>
+
+<h1 class="text-center pt-5">The EZELEA is a gaming device and only plays retro games at that. If you try to run a desktop OS like Raspberry Pi OS on the EZELEA then it will be too slow to run anything. That is why we created what you can call the UltronBook with a 24 inch display without the bottom part and without the hinge, although the real name is the Ultron Desktop. The way it is going to be made is by taking an old cracked Galaxy S10+ with DeX and putting it in a 3D-Printed desktop frame. It has 4 usb ports, USB-C charging, and a disc drive.</h1>
 
 <div class="w-full text-center">
-    <a class=" shadow-xl btn mt-6 rounded-full bg-black text-white hover:shadow-xl " href="https://www.youtube.com/channel/UCNO7sfAI3CiN59y2vDt2dVw" target="_blank">Youtube</a>
+  <a class=" poppins btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/WMS8PLIXHW84?ref_=wl_share" target="_blank">Ultron Desktop Wishlist</a>
+</div>
+
+<!-- svelte-ignore a11y-missing-content -->
+<h1 class="pt-20"></h1>
+
+<div class="w-full text-center">
+  <a class="poppins shadow-xl btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl" href="https://www.youtube.com/channel/UCNO7sfAI3CiN59y2vDt2dVw" target="_blank">Youtube</a>
 
 
-<a href="mailto:ultron.business@gmail.com?subject="  class="btn rounded-full bg-black text-white shadow-xl" target="_blank">Contact Me</a>
+<a href="mailto:ultron.business@gmail.com?subject="  class=" poppins btn glass rounded-full bg-white text-black shadow-xl" target="_blank">Contact Me</a>
 </div>
 
 <!-- svelte-ignore a11y-missing-attribute -->
 <html class="pt-8"> </html>
+
+
+
 
 
