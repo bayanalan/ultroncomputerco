1b9c16feb8cfc5b65fda6a0fed0e3cd3d6558b1f
Bayan Jones <59375928+bayanalan@users.noreply.github.com>
2022-09-25 11:19:36 -0700

    fixed buttons

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,4 @@
-<div class="navbar bg-white sticky z-50 top-2 rounded-full w-[calc(100%-20px)] mx-auto">
+<div class="navbar sticky bg-white z-50 top-2 rounded-full w-[calc(100%-20px)] mx-auto">
     <div class="navbar-center text-center mx-auto ">
       <!-- svelte-ignore a11y-missing-attribute -->
       <a class="poppins text-black text-3xl ">
@@ -9,7 +9,7 @@
     </div>
   </div>
   
- <h1 class=" poppins text-center text-6xl pt-12">About Me</h1> 
+<h1 class=" poppins text-center text-6xl pt-12">About Me</h1> 
 <p class="text-center pt-10" > My name is Bayan Jones and I am nine years old. Ultron Computer Co is my own Computer Company. I really like making computers, and that's
  why I started the Ultron Computer Co.</p>
  <h1 class="text-6xl text-center font-Roboto pt-14">UltronBook</h1>
@@ -17,16 +17,16 @@
 runs android 13 Samsung DeX, 8gb RAM, 5 megapixel camera, 128GB Built in storage, 3 fans, 8 usb ports, USB-C charging, a 3.5mm jack, and a 1TB Micro SD Card. Currently, I have the display, and the trackpad. I apologize if the Micro SD Card was $184.25 </h1>
 
 <div class=" poppins w-full text-center">
-  <a class="btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/3EDWSS4QYWYUV/ref=nav_wishlist_lists_3" target="_blank">UltronBook Wishlist</a>
+  <a class="btn glass mt-6 rounded-full hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/3EDWSS4QYWYUV/ref=nav_wishlist_lists_3" target="_blank">UltronBook Wishlist</a>
 </div>
 
 <h1 class=" poppins text-center text-6xl pt-14">PlayBox 2</h1>
 
-<h1 class="text-center pt-5">The PlayBox 2 is a gaming console almost as fast as a PS5. it is very simple, just a Galaxy S10+ with a USB C dock. it is very fast with one HDMI, one USB 3rd gen port, one USB 2nd gen port and one multi-use USB C port. It also has force landscape and console launcher.</h1>
+<h1 class="text-center pt-5">The PlayBox 2 is a gaming console as fast as a PS4 Pro. it is very simple, just a Galaxy S10+ with a USB C dock. it is very fast with one HDMI, one USB 3rd gen port, one USB 2nd gen port and one multi-use USB C port. It also has force landscape and console launcher.</h1>
 
 <div class="w-full text-center">
   <!-- svelte-ignore a11y-invalid-attribute -->
-  <a class=" poppins btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl " href="" target="https://www.amazon.com/hz/wishlist/ls/37KNG8GX9HOEZ?ref_=wl_share">PlayBox 2 Wishlist</a>
+  <a class=" poppins btn glass mt-6 rounded-full hover:shadow-xl " href="" target="https://www.amazon.com/hz/wishlist/ls/37KNG8GX9HOEZ?ref_=wl_share">PlayBox 2 Wishlist</a>
 </div>
 
 <h1 class=" poppins text-center text-6xl pt-14">PlayCube</h1>
@@ -35,17 +35,17 @@ runs android 13 Samsung DeX, 8gb RAM, 5 megapixel camera, 128GB Built in storage
 
 <div class="w-full text-center">
   <!-- svelte-ignore a11y-invalid-attribute -->
-  <a class=" poppins btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl " href="" target="https://www.amazon.com/hz/wishlist/ls/2ZO69VMW2G8LZ?ref_=wl_sharex">PlayCube Wishlist</a>
+  <a class=" poppins btn glass mt-6 rounded-full hover:shadow-xl " href="" target="https://www.amazon.com/hz/wishlist/ls/2ZO69VMW2G8LZ?ref_=wl_sharex">PlayCube Wishlist</a>
 </div>
 
 
 
 <h1 class="text-center text-6xl pt-14">Dream PC</h1>
 
-<h1 class="text-center pt-5">I included my Dream PC because I want to develop websites with code, (you need a powerful computer to do that), play games and make videos about it, (you also need a powerful computer to do that), and I want to use it for tinkercad models of Ultron computers. I already have the Aorus B450 Pro Motherboard</h1>
+<h1 class="text-center pt-5">I included my Dream PC because I want to develop websites with code, (you need a powerful computer to do that), play games and make videos about it, (you also need a powerful computer to do that), and I want to use it for tinkercad models of Ultron computers. I already have the Aorus B450 Pro Motherboard 1st gen.</h1>
 
 <div class="w-full text-center">
-  <a class=" poppins btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/W7807HTP7TBW?ref_=wl_share" target="_blank">Dream PC Wishlist</a>
+  <a class=" poppins btn glass mt-6 rounded-full hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/W7807HTP7TBW?ref_=wl_share" target="_blank">Dream PC Wishlist</a>
 </div>
  
 <h1 class="text-center text-6xl pt-14 ">Ultron Desktop </h1>
@@ -53,19 +53,18 @@ runs android 13 Samsung DeX, 8gb RAM, 5 megapixel camera, 128GB Built in storage
 <h1 class="text-center pt-5">This is what you can call the UltronBook with a 24 inch display without the bottom part and without the hinge, although the real name is the Ultron Desktop. The way it is going to be made is by taking an old cracked Galaxy S10+ with DeX and putting it in a 3D-Printed desktop frame. It has 4 usb ports, USB-C charging, and a disc drive.</h1>
 
 <div class="w-full text-center">
-  <a class=" poppins btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/WMS8PLIXHW84?ref_=wl_share" target="_blank">Ultron Desktop Wishlist</a>
+  <a class=" poppins btn glass mt-6 rounded-full hover:shadow-xl " href="https://www.amazon.com/hz/wishlist/ls/WMS8PLIXHW84?ref_=wl_share" target="_blank">Ultron Desktop Wishlist</a>
 </div>
 
 <!-- svelte-ignore a11y-missing-content -->
 <h1 class="pt-20"></h1>
 
 <div class="w-full text-center">
-  <a class="poppins shadow-xl btn glass mt-6 rounded-full bg-white text-black hover:shadow-xl" href="https://www.youtube.com/channel/UCNO7sfAI3CiN59y2vDt2dVw" target="_blank">Youtube</a>
+  <a class="btn glass poppins shadow-xl mt-6 rounded-full hover:shadow-xl" href="https://www.youtube.com/channel/UCNO7sfAI3CiN59y2vDt2dVw" target="_blank">Youtube</a>
 
 
-<a href="mailto:ultron.business@gmail.com?subject="  class=" poppins btn glass rounded-full bg-white text-black shadow-xl" target="_blank">Contact Me</a>
+<a href="mailto:ultron.business@gmail.com?subject="  class="btn glass poppins rounded-full shadow-xl" target="_blank">Contact Me</a>
 </div>
-
 
 
 <style> 
@@ -78,7 +77,3 @@ color:rgb(239, 239, 239) !important;
 .btn{z-index:1;}
 
 </style>
-
-
-
-
